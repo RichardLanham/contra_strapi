93abf4cf73c12212b7a43bcaffcb55291d97564f
Richard Lanham <richard@RichardM1ProMac.local>
2023-04-01 13:44:49 -0400

    LeadForm lifecycle add dynamic_template_date (for Sendgrid)

--- src/api/lead-form-submission/content-types/lead-form-submission/lifecycles.js
+++ src/api/lead-form-submission/content-types/lead-form-submission/lifecycles.js
@@ -6,6 +6,11 @@ module.exports = {
       await strapi.plugins["email"].services.email.send({
         to: result.email,
         template_id: "d-547dcae92cb04ca1a3234fe6b5140bd8",
+        dynamic_template_data: {
+          email: result.email,
+          name: result.name,
+          id: result.id,
+        },
       });
     } catch (err) {
       console.log(err);
