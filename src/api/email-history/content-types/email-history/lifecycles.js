37a01be4ed562a04abbe4eb56a8405d05cc5b3f0
Richard Lanham <richard@RichardM1ProMac.local>
2023-09-27 18:58:36 -0400

    dynamic template crm 2

--- src/api/email-history/content-types/email-history/lifecycles.js
+++ src/api/email-history/content-types/email-history/lifecycles.js
@@ -2,11 +2,13 @@ module.exports = {
   async afterCreate(event, data) {
     const { result } = event;
     console.log("after EMAIL HISTORy");
+    console.log(result.template);
+    // "d-2bee3b96bc8b42ada98622afe70fb820",
     try {
       await strapi.plugins["email"].services.email
         .send({
           to: result.email,
-          template_id: result.template, // "d-2bee3b96bc8b42ada98622afe70fb820",
+          template_id: result.template,
           dynamic_template_data: {
             email: result.email,
             name: result.name,
